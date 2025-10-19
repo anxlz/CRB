68a119e4ebeddfa1497614e20556e151e533e8b7
AnxiL <enqpne@gmail.com>
2025-10-19 13:15:05 +0300

    1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
-import { Module } from '@nestjs/common';
-import { NecordModule } from 'necord';
-import { BotModule } from './bot/bot.module';
+import { Module } from "@nestjs/common";
+import { NecordModule } from "necord";
+import { BotModule } from "./bot/bot.module";
 
 @Module({
   imports: [
     NecordModule.forRoot({
-      token: process.env.DISCORD_BOT_TOKEN || '',
-      intents: ['Guilds', 'GuildMessages', 'MessageContent'],
+      token: process.env.DISCORD_BOT_TOKEN || "",
+      intents: ["Guilds", "GuildMessages", "MessageContent"],
       development: process.env.DISCORD_DEVELOPMENT_GUILD_ID
         ? [process.env.DISCORD_DEVELOPMENT_GUILD_ID]
         : undefined,
